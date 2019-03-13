9be340521f000dff369c16e1e044bfd7ca2875ad
Oliver <oliver.grant@gmail.com>
2019-03-13 15:33:15 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,15 +3,15 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.last == "."
   end
 
   def question?
-
+    self.last == "?"
   end
 
   def exclamation?
-
+    self.last == "!"
   end
 
   def count_sentences
